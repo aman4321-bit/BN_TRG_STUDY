b28a0347b4b594dd67339816384b3c20a71b51a3
AMAN <amanrobotic@gmail.com>
2026-09-18 15:45:23 +0530

    Update studyLoginGitHub.js

--- studyLoginGitHub.js
+++ studyLoginGitHub.js
@@ -1,5 +1,6 @@
 function studyUserLogin() {
-    document.getElementById("username").value = "";
+
+    document.getElementById("username").value = "study";
     document.getElementById("password").value = "";
 
     document.getElementById("username").focus();
@@ -7,16 +8,18 @@ function studyUserLogin() {
 
 function login(event) {
 
-    if (event) {
-        event.preventDefault();
-    }
+    event.preventDefault();
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
 
     if (username === "study" && password === "1234") {
+
         window.location.href = "study.html";
+
     } else {
+
         alert("Invalid Username or Password");
+
     }
 }
